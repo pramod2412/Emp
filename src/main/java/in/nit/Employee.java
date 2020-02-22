c226c92cd64240b04b81abc59c66bb7b4b0e8660
pk <pramodprasad2412@gmail.com>
2020-02-22 15:51:26 +0530

    commit

---
+++ src/main/java/in/nit/Employee.java
@@ -0,0 +1,12 @@
+package in.nit;
+
+public class Employee {
+	public static void main(String[] args) {
+		
+ String ename="ABC";
+ int esal=30000;
+ System.out.println(ename);
+System.out.println(esal);
+	}
+	
+}
